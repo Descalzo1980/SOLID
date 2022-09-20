1dbd4eb5145fd2d315d2766b55b9f1c46df30cd9
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-09-20 20:44:51 +0300

    Initial commit

---
+++ src/Computer.java
@@ -0,0 +1,17 @@
+public class Computer {
+    String name;
+    int memorySize;
+
+    public Computer(String name, int memorySize) {
+        this.name = name;
+        this.memorySize = memorySize;
+    }
+
+    public void save(){
+        System.out.println("Сохранение обьекта класса в файл");
+    }
+
+    public void load(){
+        System.out.println("Загрузка обьекта класса из файла");
+    }
+}
